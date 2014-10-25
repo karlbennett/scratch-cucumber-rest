32b33b6f83f2f6ec9c737d9cd8d447b46f730dca
karlbennett <where_1ts@yahoo.com>
2014-10-25 14:23:40 +1300

    Added extra tests for the VerboseResponse class.

--- src/test/java/scratch/cucumber/rest/steps/VerboseResponseTest.java
+++ src/test/java/scratch/cucumber/rest/steps/VerboseResponseTest.java
@@ -1,12 +1,17 @@
 package scratch.cucumber.rest.steps;
 
+import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.jersey.client.ClientRequest;
 import org.glassfish.jersey.client.ClientResponse;
 import org.junit.Test;
 
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Link;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.NewCookie;
+import java.lang.annotation.Annotation;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Map;
 import java.util.Set;
 
@@ -16,6 +21,8 @@ import static java.util.Map.Entry;
 import static javax.ws.rs.core.Response.StatusType;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class VerboseResponseTest {
@@ -90,58 +97,134 @@ public class VerboseResponseTest {
     }
 
     @Test
-    public void testGetResolvedRequestUri() {
+    public void testGetResolvedRequestUri() throws URISyntaxException {
 
+        final URI uri = new URI("");
+
+        final ClientResponse response = mockResponse();
+        when(response.getResolvedRequestUri()).thenReturn(uri);
+
+        assertEquals("get uri should delegate.", uri, new VerboseResponse(response).getResolvedRequestUri());
     }
 
     @Test
     public void testGetServiceLocator() {
 
+        final ServiceLocator serviceLocator = mock(ServiceLocator.class);
+
+        final ClientResponse response = mockResponse();
+        when(response.getServiceLocator()).thenReturn(serviceLocator);
+
+        assertEquals("get serviceLocator should delegate.", serviceLocator,
+                new VerboseResponse(response).getServiceLocator());
     }
 
     @Test
     public void testGetStatus() {
 
+        final int status = 1;
+
+        final ClientResponse response = mockResponse();
+        when(response.getStatus()).thenReturn(status);
+
+        assertEquals("get status should delegate.", status, new VerboseResponse(response).getStatus());
     }
 
     @Test
     public void testGetStatusInfo() {
 
+        final StatusType statusType = mock(StatusType.class);
+
+        final ClientResponse response = mockResponse();
+        when(response.getStatusInfo()).thenReturn(statusType);
+
+        assertEquals("get statusType should delegate.", statusType, new VerboseResponse(response).getStatusInfo());
     }
 
     @Test
     public void testReadEntityWithClass() {
 
+        final String string = "test entity value";
+
+        final ClientResponse response = mockResponse();
+        when(response.readEntity(String.class)).thenReturn(string);
+
+        assertEquals("get entity should delegate.", string, new VerboseResponse(response).readEntity(String.class));
     }
 
     @Test
     public void testReadEntityWithClassAndAnnotations() {
 
+        final Annotation[] annotations = {};
+        final String string = "test entity value";
+
+        final ClientResponse response = mockResponse();
+        when(response.readEntity(String.class, annotations)).thenReturn(string);
+
+        assertEquals("get entity should delegate.", string,
+                new VerboseResponse(response).readEntity(String.class, annotations));
     }
 
     @Test
     public void testReadEntityWithGenericType() {
 
+        final GenericType type = mock(GenericType.class);
+        final Object entity = new Object();
+
+        final ClientResponse response = mockResponse();
+        when(response.readEntity(type)).thenReturn(entity);
+
+        assertEquals("get entity should delegate.", entity, new VerboseResponse(response).readEntity(type));
     }
 
     @Test
     public void testReadEntityGenericTypeAndAnnotations() {
 
+        final Annotation[] annotations = {};
+        final GenericType type = mock(GenericType.class);
+        final Object entity = new Object();
+
+        final ClientResponse response = mockResponse();
+        when(response.readEntity(type, annotations)).thenReturn(entity);
+
+        assertEquals("get entity should delegate.", entity,
+                new VerboseResponse(response).readEntity(type, annotations));
     }
 
     @Test
-    public void testSetResolvedRequestUri() {
+    public void testSetResolvedRequestUri() throws URISyntaxException {
 
+        final URI uri = new URI("");
+
+        final ClientResponse response = mockResponse();
+
+        new VerboseResponse(response).setResolvedRequestUri(uri);
+
+        verify(response, times(1)).setResolvedRequestUri(uri);
     }
 
     @Test
     public void testSetStatus() {
 
+        final int status = 1;
+
+        final ClientResponse response = mockResponse();
+
+        new VerboseResponse(response).setStatus(status);
+
+        verify(response, times(1)).setStatus(status);
     }
 
     @Test
     public void testSetStatusInfo() {
 
+        final StatusType status = mock(StatusType.class);
+
+        final ClientResponse response = mockResponse();
+
+        new VerboseResponse(response).setStatusInfo(status);
+
+        verify(response, times(1)).setStatusInfo(status);
     }
 
     @SuppressWarnings("unchecked")
